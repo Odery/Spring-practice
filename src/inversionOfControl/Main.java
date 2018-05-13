d9fd1d8e62455edf7f3d7804a5e5c46f4617f92c
Vakamisu <kammurimini@gmail.com>
2018-05-13 22:18:42 +0300

    Main clean up

--- src/inversionOfControl/Main.java
+++ src/inversionOfControl/Main.java
@@ -6,11 +6,7 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Student student = new Student(context.getBean("mathTeacher", ITeacher.class));
-        Student student2 = new Student(context.getBean("mathTeacher", ITeacher.class));
 
-        System.out.println(student.getTeacher() == student2.getTeacher());
-        System.out.println(student.getTeacher());
-        System.out.println(student2.getTeacher());
         context.close();
     }
 }
