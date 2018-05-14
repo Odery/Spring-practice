0deeb2674c0f03f12330f875a32137e80bea4311
Vakamisu <kammurimini@gmail.com>
2018-05-14 23:55:47 +0300

    Main cleanUp

--- src/annotationConfig/Main.java
+++ src/annotationConfig/Main.java
@@ -7,11 +7,7 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
         ITeacher teacher = context.getBean("javaTeacher", ITeacher.class);
-        ITeacher teacher2 = context.getBean("javaTeacher", ITeacher.class);
 
-        System.out.println("Objects the same?: " + (teacher == teacher2));
-        System.out.println("Cell in memory: " + teacher);
-        System.out.println("Cell in memory: " + teacher2);
         context.close();
     }
 
